4baac526f3c820c4d7dcdb7b212aa539fa941446
IsmaDavidAA <idaasannmvgd@gmail.com>
2021-11-27 11:02:08 -0400

    agregar nombre

---
+++ src/types/clase.js
@@ -0,0 +1,10 @@
+export const ClaseModel = {
+  x: 0,
+  y: 0,
+  color: "",
+  width: 0,
+  height: 0,
+  name: "",
+  isInRangeY: (number) => {},
+  isInRangeX: (number) => {},
+};
--- src/views/CanvasView/CanvasView.js
+++ src/views/CanvasView/CanvasView.js
@@ -4,30 +4,31 @@ import DashBoard from "../../components/Dashboard/Dashboard";
 import Header from "../../components/Header/Header";
 import { WrapperView } from "../../GlobalStyle";
 import { WrapperCanvas, WrapperDescktop } from "./CanvasView.styles";
+import { ClaseModel } from "../../types/clase";
 const CanvasView = (props) => {
   const [clases, setClases] = useState([]);
   const [inicioX, setInicioX] = useState(0);
   const [inicioY, setInicioY] = useState(0);
-  const [claseActual, setClaseActual] = useState(null);
   const [cv, setCv] = useState(document.getElementById("canvas"));
   const [cx, setCx] = useState();
-  const vertical = 50;
-  const horizontal = 120;
+  const VERTICAL = 50;
+  const HORIZONTAL = 120;
   var objetoActual = null;
 
   function actualizar() {
     cx.fillStyle = "#f0f0f0";
     cx.fillRect(0, 0, 900, 550);
     for (var i = 0; i < clases.length; i++) {
+      cx.font = "22px Arial";
       cx.fillStyle = clases[i].color;
       cx.fillRect(clases[i].x, clases[i].y, clases[i].width, clases[i].height);
+      cx.fillStyle = "black";
+      cx.fillText(clases[i].name, clases[i].x, clases[i].y + clases[i].height);
     }
   }
   useEffect(() => {
-    // if (document.getElementById("canvas")) {
     setCv(document.getElementById("canvas"));
     console.log("wololo");
-    // }
   }, []);
   useEffect(() => {
     if (cv) {
@@ -35,48 +36,48 @@ const CanvasView = (props) => {
     }
   }, [cv]);
   const dibujar = () => {
-    clases.push({ x: 10, y: 10, width: 40, height: 20, color: "green" });
-  };
-
-  window.onload = () => {
     clases.push({
       x: 10,
       y: 10,
-      width: 10,
-      height: 20,
-      color: "#00f",
+      width: 80,
+      height: 50,
+      color: "green",
+      name: "Perro",
     });
+  };
+
+  window.onload = () => {
     actualizar();
 
     cv.onmousedown = function (event) {
-      for (var i = 0; i < clases.length; i++) {
-        if (
-          clases[i].x < event.clientX - horizontal &&
-          clases[i].width + clases[i].x > event.clientX - horizontal &&
-          clases[i].y < event.clientY - vertical &&
-          clases[i].height + clases[i].y > event.clientY - vertical
-        ) {
-          setClaseActual(clases[i]);
-          objetoActual = clases[i];
-          setInicioY(event.clientY - clases[i].y - vertical);
-          setInicioX(event.clientX - clases[i].x - horizontal);
-          break;
+      if (event.button === 0) {
+        for (var i = 0; i < clases.length; i++) {
+          if (
+            clases[i].x < event.clientX - HORIZONTAL &&
+            clases[i].width + clases[i].x > event.clientX - HORIZONTAL &&
+            clases[i].y < event.clientY - VERTICAL &&
+            clases[i].height + clases[i].y > event.clientY - VERTICAL
+          ) {
+            objetoActual = clases[i];
+            setInicioY(event.clientY - clases[i].y - VERTICAL);
+            setInicioX(event.clientX - clases[i].x - HORIZONTAL);
+            break;
+          }
         }
       }
     };
 
     cv.onmousemove = function (event) {
       if (objetoActual != null) {
-        let claseTemp = claseActual;
-        objetoActual.x = event.clientX - inicioX - horizontal;
-        objetoActual.y = event.clientY - inicioY - vertical;
-        setClaseActual(claseTemp);
+        objetoActual.x =
+          event.clientX - inicioX - HORIZONTAL - objetoActual.width / 2;
+        objetoActual.y =
+          event.clientY - inicioY - VERTICAL - objetoActual.height / 2;
       }
       actualizar();
     };
 
     cv.onmouseup = function (event) {
-      setClaseActual(null);
       objetoActual = null;
     };
   };
