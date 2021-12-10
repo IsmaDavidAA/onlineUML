1be85df7a4d19462e789de40a38383a733707ef0
amorraga <201809256@est.umss.edu>
2021-12-10 17:40:51 -0400

    Agregar coordenadas

--- src/utils/Calculator.js
+++ src/utils/Calculator.js
@@ -1,4 +1,52 @@
 import { HORIZONTAL, VERTICAL } from "../Constants";
+
+const calculatorDireccion = (claseA, claseB) => {
+  var coorA = calculatorCoordenada(claseA);
+  var coorB = calculatorCoordenada(claseB);
+  if(interseccion(claseA, claseB)){
+    if(claseA.y>claseB.y){
+      return {origen: coorA.n, destino: coorB.s};
+    }else if(claseA.x < claseB.x){
+      return {origen: coorA.e, destino: coorB.o};
+    }else if(claseA.y < claseB.y){
+      return {origen: coorA.n, destino: coorB.s};
+    }else if(claseA.x>claseB.x){
+      return {origen: coorA.o, destino: coorB.e};
+    }
+  }else if(claseA.y > claseB.y){
+    if(claseA.x > claseB.x){
+      return {origen: coorA.s, destino: coorB.e};
+    }else if(claseA.x < claseB){
+      return {origen: coorA.s, destino: coorB.e};
+    }
+  }else if(claseA.y < claseB.y){
+    if(claseA.x > claseB.x){
+      return {origen: claseA.s, destino: claseB.e};
+    }else if(claseA.x < claseB){
+      return {origen: claseA.s, destino: claseB.e};
+    }
+  }
+};
+function interseccion(claseA, claseB){
+  if(claseA.x < claseB.x){
+    return true;
+  }else if(claseA.y<claseB.y){
+    return true;
+  }else{
+    return false;
+  }
+}
+var claseP={height: 54, width: 60, x: 314, y: 278};
+var clase2={height: 86, width: 72, x: 192, y: 149};
+console.log(calculatorCoordenada(claseP));
+
+function calculatorCoordenada(claseA){
+  var PtoNorte = {x: claseA.x + claseA.width/2, y: claseA.y+claseA.height};
+  var PtoSur = {x: claseA.y + claseA.height, y: claseA.x + claseA.width/2};
+  var PtoOeste = {x: claseA.x, y: claseA.y + claseA.height/2};
+  var PtoEste = {x: claseA.x, y: claseA.y+claseA.height/2};
+  return {n: PtoNorte, s: PtoSur, o:PtoOeste, e:PtoEste};
+};
 export const calculator = {
   calculateHeightClass: (attributes, methods) => {
     return 22 + 16 * attributes.length + 16 * methods.length;
